881fb30fc1a29d8634f323684dbb7d4f426b3502
Tim Smith <tim@cozy.co>
2015-07-12 01:17:59 -0700

    Consistent capitalization

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ recipe            'openldap',         'Empty, use one of the other recipes'
 recipe            'openldap::auth',   'Set up openldap for user authentication'
 recipe            'openldap::client', 'Install openldap client packages'
 recipe            'openldap::server', 'Set up openldap to be a slapd server'
-recipe            'openldap::slave',  'uses search to set replication slave attributes'
+recipe            'openldap::slave',  'Uses search to set replication slave attributes'
 recipe            'openldap::master', 'Use on nodes that should be a slapd master'
 
 %w(ubuntu debian freebsd redhat centos amazon scientific oracle).each do |os|
