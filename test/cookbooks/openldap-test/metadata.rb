1633585aa403bd54d0587c9172cacb22884131cd
Tim Smith <tsmith@chef.io>
2016-07-19 15:48:07 -0700

    Slim down the test recipe
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/openldap-test/metadata.rb
+++ test/cookbooks/openldap-test/metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Tests various aspects of the OpenLDAP cookbook'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'openldap'
