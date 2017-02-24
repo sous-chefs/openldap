3f5636dc434dcdacb3755de36282ad8131f9d63e
Tim Smith <tsmith@chef.io>
2017-02-23 20:17:00 -0800

    Cookstyle fixes and Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/openldap-test/recipes/ssl.rb
+++ test/cookbooks/openldap-test/recipes/ssl.rb
@@ -3,19 +3,19 @@ hostsfile_entry '127.0.1.1' do
   action :append
 end
 
-directory node[:openldap][:ssl_dir] do
+directory node['openldap']['ssl_dir'] do
   owner 'root'
   group 'root'
-  mode 00755
+  mode '755'
   action :create
 end
 
-node[:certs].each do |c|
+node['certs'].each do |c|
   cookbook_file c do
     backup false
     action :create_if_missing
     owner 'root'
     group 'root'
-    mode 0644
+    mode '644'
   end
 end
