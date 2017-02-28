e97ec74ccd94975f4d36ea9128e2984b34e2b2ee
Tim Smith <tsmith@chef.io>
2017-02-28 13:46:59 -0800

    Update testing for Chef 13
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -17,4 +17,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/openldap'
 issues_url 'https://github.com/chef-cookbooks/openldap/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
