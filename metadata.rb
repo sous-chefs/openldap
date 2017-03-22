08e4723375bcf464eb9ccaa3dabe1f6c0147023c
Tim Smith <tsmith@chef.io>
2017-03-21 21:07:14 -0700

    Use standardize Apache 2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'openldap'
 maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs and configures OpenLDAP (slapd)'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '3.0.0'
--- test/cookbooks/openldap-test/metadata.rb
+++ test/cookbooks/openldap-test/metadata.rb
@@ -1,7 +1,7 @@
 name             'openldap-test'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Tests various aspects of the OpenLDAP cookbook'
 version          '0.1.0'
 
