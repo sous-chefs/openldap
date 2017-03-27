96b5c044fe8f8d82bba055d58b36628b8be99efa
Jennifer Davis <iennae@gmail.com>
2017-03-27 13:19:49 -0700

    Update description in metadata.rb to improve supermarket search query for ldap.
    Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              'openldap'
 maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
-description       'Installs and configures OpenLDAP (slapd)'
+description       'Installs and configures OpenLDAP (slapd) an open source implementation of LDAP.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '3.0.0'
 
