f658812c46fa460fb7f3197c40f694075466b2ba
Tim Smith <tsmith@chef.io>
2018-05-15 19:45:11 -0700

    Update ChefSpecs to the latest platform releases
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -22,7 +22,7 @@ describe 'default recipe on ubuntu 16.04' do
 end
 
 describe 'default recipe on centos 7' do
-  let(:runner) { ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511', step_into: ['openldap_install']) }
+  let(:runner) { ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611', step_into: ['openldap_install']) }
   let(:chef_run) { runner.converge('openldap::default') }
 
   it 'converges successfully' do
@@ -39,7 +39,7 @@ describe 'default recipe on centos 7' do
 end
 
 describe 'default recipe on centos 6' do
-  let(:runner) { ChefSpec::ServerRunner.new(platform: 'centos', version: '6.8', step_into: ['openldap_install']) }
+  let(:runner) { ChefSpec::ServerRunner.new(platform: 'centos', version: '6.9', step_into: ['openldap_install']) }
   let(:chef_run) { runner.converge('openldap::default') }
 
   it 'converges successfully' do
