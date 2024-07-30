a755054b98ae0c7521dfa20edf5cb1018a285822
Steve Yun <syun@etsy.com>
2024-07-30 16:18:40 -0400

    added tests

---
+++ test/integration/accesslog/controls/accesslog_spec.rb
@@ -0,0 +1,21 @@
+ldap_dir =
+  case os.family
+  when 'debian'
+    '/etc/ldap'
+  when 'redhat', 'amazon', 'fedora', 'suse'
+    '/etc/openldap'
+  when 'bsd'
+    '/usr/local/etc/openldap'
+  end
+
+control 'accesslog' do
+  describe file "#{ldap_dir}/slapd.conf" do
+    its('content') { should match /# accesslog configuration/ }
+    its('content') { should match /overlay accesslog/ }
+  end
+  
+  describe service('slapd') do
+    it { should be_installed }
+    it { should be_running }
+  end
+end
