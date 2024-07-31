9af11afcc5100eb7c57fdfaa3ac186e95ee63a32
Steve Yun <syun@etsy.com>
2024-07-31 13:20:25 -0400

    fix lint space

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -4,6 +4,6 @@ node.default['openldap']['accesslog']['directory'] = '/var/log/'
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
-node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00' 
+node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 include_recipe 'openldap::default'
