2d29e6531f7c74892ded5a8940e327fac16a4337
Steve Yun <syun@etsy.com>
2024-08-01 10:54:45 -0400

    trying a diff user/group

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -1,6 +1,6 @@
 node.default['openldap']['accesslog']['enabled'] = true
 node.default['openldap']['accesslog']['logdb'] = '"cn=accesslog"'
-node.default['openldap']['accesslog']['directory'] = '/var/lib/ldap'
+node.default['openldap']['accesslog']['directory'] = '/var/lib/ldap/accesslog'
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
@@ -8,11 +8,15 @@ node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 node.default['openldap']['modules'] << 'accesslog'
 
-directory '/var/lib/ldap' do
-  mode '0755'
-  owner 'openldap'
-  group 'openldap'
-  action :create
+directories = ['/var/lib/ldap', '/var/lib/ldap/accesslog']
+
+directories.each do |dir|
+  directory dir do  
+    mode '0755'
+    owner 'root'
+    group 'root'
+    action :create
+  end
 end
 
 include_recipe 'openldap::default'
