00c6e1ed5b6c205e470758f09c848d3624a7aa82
Steve Yun <syun@etsy.com>
2024-08-01 14:15:45 -0400

    moved accesslog for test

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -1,6 +1,16 @@
+ldap_dir =
+  case os.family
+  when 'debian'
+    '/etc/ldap'
+  when 'redhat', 'amazon', 'fedora', 'suse'
+    '/etc/openldap'
+  when 'bsd'
+    '/usr/local/etc/openldap'
+  end
+
 node.default['openldap']['accesslog']['enabled'] = true
 node.default['openldap']['accesslog']['logdb'] = '"cn=accesslog"'
-node.default['openldap']['accesslog']['directory'] = '/var/lib/ldap/accesslog'
+node.default['openldap']['accesslog']['directory'] = '#{ldap_dir}/accesslog'
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
@@ -8,10 +18,7 @@ node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 node.default['openldap']['modules'] << 'accesslog'
 
-directories = ['/var/lib/ldap', '/var/lib/ldap/accesslog']
-
-directories.each do |dir|
-  directory dir do
+directory '#{ldap_dir}/accesslog' do
     mode '0755'
     owner 'root'
     group 'root'
