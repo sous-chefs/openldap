7879af8ffba40bd9007cba5a77ef1062797b90a0
Steve Yun <syun@etsy.com>
2024-08-01 14:55:24 -0400

    proper quotes

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -10,7 +10,7 @@ ldap_dir =
 
 node.default['openldap']['accesslog']['enabled'] = true
 node.default['openldap']['accesslog']['logdb'] = '"cn=accesslog"'
-node.default['openldap']['accesslog']['directory'] = '#{ldap_dir}/accesslog'
+node.default['openldap']['accesslog']['directory'] = "#{ldap_dir}/accesslog"
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
@@ -18,7 +18,7 @@ node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 node.default['openldap']['modules'] << 'accesslog'
 
-directory '#{ldap_dir}/accesslog' do
+directory "#{ldap_dir}/accesslog" do
   mode '0755'
   owner 'openldap'
   group 'openldap'
