0daff1d58e493b55df5805f5d941c9a4cba46cda
Steve Yun <syun@etsy.com>
2024-08-01 10:37:38 -0400

    added directory for accesslog for test

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -1,11 +1,18 @@
 node.default['openldap']['accesslog']['enabled'] = true
 node.default['openldap']['accesslog']['logdb'] = '"cn=accesslog"'
-node.default['openldap']['accesslog']['directory'] = '/var/log/'
+node.default['openldap']['accesslog']['directory'] = '/var/lib/ldap'
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
 node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 node.default['openldap']['modules'] << 'accesslog'
+
+directory '/var/lib/ldap' do
+  mode '0755'
+  owner 'openldap'
+  group 'openldap'
+  action :create
+end
 
 include_recipe 'openldap::default'
