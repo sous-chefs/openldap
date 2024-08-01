13bb656819643e720a991fcd2f0484226dc24bf6
Steve Yun <syun@etsy.com>
2024-08-01 15:10:04 -0400

    single quote

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -1,6 +1,6 @@
 node.default['openldap']['accesslog']['enabled'] = true
 node.default['openldap']['accesslog']['logdb'] = '"cn=accesslog"'
-node.default['openldap']['accesslog']['directory'] = "/etc/ldap/accesslog"
+node.default['openldap']['accesslog']['directory'] = '/etc/ldap/accesslog'
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
@@ -8,7 +8,7 @@ node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 node.default['openldap']['modules'] << 'accesslog'
 
-directory "/etc/ldap/accesslog" do
+directory '/etc/ldap/accesslog' do
   mode '0755'
   owner 'openldap'
   group 'openldap'
