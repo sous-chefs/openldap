64d7efb8c0f23d1deff70885bf97b01f5d9afed3
Steve Yun <syun@etsy.com>
2024-08-02 08:58:47 -0400

    trying something

--- test/cookbooks/openldap-test/recipes/accesslog.rb
+++ test/cookbooks/openldap-test/recipes/accesslog.rb
@@ -1,11 +1,30 @@
 node.default['openldap']['accesslog']['enabled'] = true
 node.default['openldap']['accesslog']['logdb'] = '"cn=accesslog"'
-node.default['openldap']['accesslog']['directory'] = '/etc/ldap/'
+node.default['openldap']['accesslog']['directory'] = '/var/lib/ldap/accesslog'
 node.default['openldap']['accesslog']['index'] = 'reqStart,reqEnd,reqResult eq'
 node.default['openldap']['accesslog']['logops'] = 'writes'
 node.default['openldap']['accesslog']['logold'] = '(objectclass=*)'
 node.default['openldap']['accesslog']['logpurge'] = '8+00:00 1+00:00'
 
 node.default['openldap']['modules'] << 'accesslog'
+
+user 'openldap' do
+  action :create
+end
+
+group 'openldap' do
+  action :create
+end
+
+directories = ['/var/lib/ldap', '/var/lib/ldap/accesslog']
+
+directories.each do |dir|
+  directory dir do
+    mode '0755'
+    owner 'openldap'
+    group 'openldap'
+    action :create
+  end
+end
 
 include_recipe 'openldap::default'
